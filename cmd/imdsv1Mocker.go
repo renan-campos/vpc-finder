0d9c5ab8c740cc0cb0515cdf59a2769f9f7a95af
Renan Campos <rcampos@redhat.com>
2022-04-01 17:29:31 -0400

    WIP: Getting a test working in minikube
    Current issue: Unable to point the vpc-checker pod to the mock server.

--- cmd/imdsv1Mocker.go
+++ cmd/imdsv1Mocker.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+)
+
+const (
+	listenAddr = ":8080"
+	macPath    = "/latest/meta-data/mac"
+	cidrPath   = "/latest/meta-data/network/interfaces/macs/ff:ff:ff:ff:ff:ff/vpc-ipv4-cidr-block"
+	fakeMac    = "ff:ff:ff:ff:ff:ff"
+	fakeCIDR   = "10.0.0.0/16"
 )
 
 func main() {
-	fmt.Printf("TODO\n")
+	http.HandleFunc(macPath, func(writer http.ResponseWriter, req *http.Request) {
+		fmt.Println("Received request for mac address")
+		_, err := writer.Write([]byte(fakeMac))
+		if err != nil {
+			fmt.Fprintf(os.Stderr,
+				"Failed to write response body: %s", err)
+			writer.WriteHeader(http.StatusServiceUnavailable)
+		}
+	})
+
+	http.HandleFunc(cidrPath, func(writer http.ResponseWriter, req *http.Request) {
+		fmt.Println("Received request for vpc cidr")
+		_, err := writer.Write([]byte(fakeCIDR))
+		if err != nil {
+			fmt.Fprintf(os.Stderr,
+				"Failed to write response body: %s", err)
+			writer.WriteHeader(http.StatusServiceUnavailable)
+		}
+	})
+
+	fmt.Println("Starting fake IMDS v1 server...")
+	http.ListenAndServe(listenAddr, nil)
 }
